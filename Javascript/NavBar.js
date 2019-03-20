990ea930cb149f1ef142a10fdab72f6e6a94f613
zkang35 <harrydexiong@gmail.com>
2019-03-20 19:13:24 -0400

    text shadow & drop down button

--- Javascript/NavBar.js
+++ Javascript/NavBar.js
@@ -1,10 +1,21 @@
+var div = document.getElementsByClassName('dropdown');
+var icon = document.getElementsByClassName('fa-sort-down');
+var open = false;
+
+
 function dropdown() {
     document.getElementById("theDropDown").classList.toggle("show");
+    if (!open) {
+        icon[0].classList.toggle("open");
+    }
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = (e) => {
-    if (!e.target.matches('.dropdownbtn')) {
+    if (!e.target.matches('.dropdownbtn') && !e.target.matches('.fa.fa-sort-down.open')) {
+        if (!open) {
+            icon[0].classList.remove("open");
+        }
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
